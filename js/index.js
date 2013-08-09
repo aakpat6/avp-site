5147d54dfaac341fc99bd8bc4d7a0dd25dc9b4e0
Aakash Patel <aakpat6@gmail.com>
2013-08-09 09:24:27 -0700

    Update puzzles section.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 function AvpSite() {}
 
+AvpSite.updateParallax = function() {
+  var top = $(window).scrollTop();
+  // $('.masthead').css('-webkit-transform', 'translateY(' + (top / 2) + 'px)');
+};
+
 AvpSite.init = function() {
   $('[rel=tooltip]').tooltip({placement: 'bottom'});
 
@@ -17,19 +22,23 @@ AvpSite.init = function() {
     }, 500);
   });
 
-  $('.masthead').each(function(){
-    var bgobj = $(this); // assigning the object
-
-    $(window).scroll(function() {
-      var yPos = -($(this).scrollTop() / bgobj.data('speed')) - bgobj.height() - 80;
-      var coords = '130px '+ yPos + 'px';
-      bgobj.css('backgroundPosition', coords);
-    });
+  AvpSite.updateParallax();
+  $(window).scroll(function() {
+    AvpSite.updateParallax();
   });
 
-  var yPos = -($(window).scrollTop() / $('.masthead').data('speed')) - $('.masthead').height() - 80;
-  var coords = '130px '+ yPos + 'px';
-  $('.masthead').css('backgroundPosition', coords);
+  // $('.masthead').each(function(){
+  //   var bgobj = $(this); // assigning the object
+  //   $(window).scroll(function() {
+  //     var yPos = -($(this).scrollTop() / bgobj.data('speed')) - bgobj.height() - 80;
+  //     var coords = '130px '+ yPos + 'px';
+  //     bgobj.css('backgroundPosition', coords);
+  //   });
+  // });
+
+  // var yPos = -($(window).scrollTop() / $('.masthead').data('speed')) - $('.masthead').height() - 80;
+  // var coords = '130px '+ yPos + 'px';
+  // $('.masthead').css('backgroundPosition', coords);
 };
 
 $(function() {
